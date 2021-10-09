90bb9244410ae450d8414e9169b61c155dc59e80
Jose Nortes <nooors@gmail.com>
2021-10-09 17:27:39 +0200

    Initial commit

---
+++ components/Fill.js
@@ -0,0 +1,12 @@
+Vue.component('Fill', {
+    template: '#fill-template',
+    props: {
+        name:''
+    },
+    data(){
+        return{
+            counter: 0
+        }
+    }
+    
+});
---
+++ components/Pare.js
@@ -0,0 +1,9 @@
+Vue.component('Pare', {
+    template: '#pare-template',
+    data() {
+        return {
+            items: ["item 1", "item 2", "item 3"]
+        }
+    }
+
+});
